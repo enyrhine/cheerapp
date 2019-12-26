5b8253ed3c8c691db6f2c11a05f152997e3857e5
Elsa Nyrhinen <elsa@vertics.co>
2019-12-26 18:01:01 +0200

    Changed Cupertino to material tabs

--- lib/team_view.dart
+++ lib/team_view.dart
@@ -2,52 +2,64 @@ import 'package:Cheerapp/model/team.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TeamView extends StatelessWidget {
+class TeamView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TeamViewState();
+}
+
+class _TeamViewState extends State<TeamView> {
+  PageController _teamPage = PageController(initialPage: 1);
   @override
   Widget build(BuildContext context) {
     final Team team = ModalRoute.of(context).settings.arguments;
-    return CupertinoTabScaffold(
-      // appBar: AppBar(
-      //   title: Text(team.name),
-      //   backgroundColor: team.color,
-      // ),
-      tabBar: CupertinoTabBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.stars)
-            )
-          ],
-          //Row(
-          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //   children: <Widget>[
-          //     IconButton(
-          //       icon: Icon(Icons.people),
-          //       color: Colors.white,
-          //       onPressed: () {},
-          //     ),
-          //     IconButton(
-          //       icon: Icon(Icons.home),
-          //       color: Colors.white,
-          //       onPressed: () {},
-          //     ),
-          //     IconButton(
-          //       icon: Icon(Icons.stars),
-          //       color: Colors.white,
-          //       onPressed: () {},
-          //     ),
-          //   ],
-          // )
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(team.name),
+            backgroundColor: team.color,
           ),
-          tabBuilder: (BuildContext context, int i) {
-            return CupertinoPageScaffold(child: CupertinoTheme(data: CupertinoThemeData(), child: Center(child: Text('Moi $i!'))));
-          },
-      // body: Center(child: Text('höpöhöpö')),
-    );
+          bottomNavigationBar: BottomAppBar(
+              color: team.color,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.people),
+                    onPressed: () {
+                      setState(() {
+                        _teamPage.jumpToPage(0);
+                      });
+                    },
+                  ),
+                  IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.home),
+                    onPressed: () {
+                      setState(() {
+                        _teamPage.jumpToPage(1);
+                      });
+                    },
+                  ),
+                  IconButton(
+                    color: Colors.white,
+                    icon: Icon(Icons.stars),
+                    onPressed: () {
+                      setState(() {
+                        _teamPage.jumpToPage(2);
+                      });
+                    },
+                  )
+                ],
+              )),
+          body: PageView(
+            controller: _teamPage,
+            onPageChanged: (int) {
+              print('Page Changes to index $int');
+            },
+            children: <Widget>[Center(child: Text('1')), Center(child: Text('2')), Center(child: Text('3'))],
+          ),
+        ));
   }
 }
